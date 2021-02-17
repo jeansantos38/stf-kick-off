59300c35641feabe60c99d78bc8bb15973cef3a8
Giacomin <giacomin@hp.com>
2021-02-17 10:35:52 -0300

    Adding more logs to asset when it fails

--- src/test/java/misc/CalendarTests.java
+++ src/test/java/misc/CalendarTests.java
@@ -16,7 +16,10 @@ public class CalendarTests extends MainTestBase {
     public void convertTimestampToDateTest() {
         String extractedDate = convertTimestampToDate(Long.valueOf("1564800583569"));
         //The results depends on where it runs.
-        Assert.assertTrue(extractedDate.equals("08/02/2019 23:49:43") || extractedDate.equals("08/03/2019 02:49:43"));
+        String expected1 = "08/02/2019 23:49:43";
+        String expected2 = "08/03/2019 02:49:43";
+        Assert.assertTrue(extractedDate.equals(expected1) || extractedDate.equals(expected2),
+                String.format("Expected [%s] or [%s], but got %s", expected1, expected2, extractedDate));
     }
 
     @Test
@@ -26,7 +29,10 @@ public class CalendarTests extends MainTestBase {
     @Link(name = "This could be a Link to your project Issue Tracker", url = "https://github.com/HPInc/smart-test-framework")
     public void convertTimestampUsingPattern() {
         String extractedDate = convertTimestampToDate(Long.valueOf("1564800583569"), "yyyy/dd/MM' 'HH:mm:ss");
+        String expected1 = "2019/02/08 23:49:43";
+        String expected2 = "2019/02/08 02:49:43";
         //The results depends on where it runs.
-        Assert.assertTrue(extractedDate.equals("2019/02/08 23:49:43") || extractedDate.equals("2019/02/08 02:49:43"));
+        Assert.assertTrue(extractedDate.equals(expected1) || extractedDate.equals(expected2),
+                String.format("Expected [%s] or [%s], but got %s", expected1, expected2, extractedDate));
     }
 }
