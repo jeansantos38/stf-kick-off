56953a6dec4442c97ae48a1c52a0158fe8d0733e
Giacomin <giacomin@hp.com>
2021-02-22 18:28:57 -0300

    wip2 gha

--- gitHubActionsScripts/setupChrome.sh
+++ gitHubActionsScripts/setupChrome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-wget https://www.dropbox.com/s/yd8w6q8jccomiev/chromedriver_linux64.zip?dl=1
+wget -O chromedriver_linux64.zip https://www.dropbox.com/s/yd8w6q8jccomiev/chromedriver_linux64.zip?dl=1
 ls -la
 pwd
 unzip chromedriver_linux64.zip
@@ -9,10 +9,11 @@ chmod 755 chromedriver
 
 
 echo "starting step1!"
-wget https://www.dropbox.com/s/l84qhhfa4q5d59c/google-chrome-stable_current_amd64.deb?dl=1
+wget -O google-chrome-stable_current_amd64.deb https://www.dropbox.com/s/l84qhhfa4q5d59c/google-chrome-stable_current_amd64.deb?dl=1
 ls
+sudo apt install google-chrome-stable_current_amd64.deb
 echo "step1 completed!"
 echo "starting step2!"
-sudo apt install google-chrome-stable_current_amd64.deb
+
 echo "step2 completed!"
 
