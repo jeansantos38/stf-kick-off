00309bc1b6f11a3c10bc5018a39440549c557d8c
Giacomin <giacomin@hp.com>
2021-02-22 18:31:33 -0300

    wip3 gha

--- gitHubActionsScripts/setupChrome.sh
+++ gitHubActionsScripts/setupChrome.sh
@@ -11,7 +11,7 @@ chmod 755 chromedriver
 echo "starting step1!"
 wget -O google-chrome-stable_current_amd64.deb https://www.dropbox.com/s/l84qhhfa4q5d59c/google-chrome-stable_current_amd64.deb?dl=1
 ls
-sudo apt install google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 echo "step1 completed!"
 echo "starting step2!"
 
