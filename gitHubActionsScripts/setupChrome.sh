762956eae118ac9d74617a43fa75c22f6014a4d8
Giacomin <giacomin@hp.com>
2021-02-22 18:52:25 -0300

    wip github actions complete!

--- gitHubActionsScripts/setupChrome.sh
+++ gitHubActionsScripts/setupChrome.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 set -ex
-
+echo "Downloading chromedriver for v.88!"
 wget -O chromedriver_linux64.zip https://www.dropbox.com/s/yd8w6q8jccomiev/chromedriver_linux64.zip?dl=1
-ls -la
-pwd
+echo "Unziping chromedriver!"
 unzip chromedriver_linux64.zip
+echo "Changing chromedriver permissions!"
 chmod 755 chromedriver
+echo "Chromedriver setup is complete!"
 
-
-echo "starting step1!"
+echo "Downloading chrome browser v.88!"
 wget -O google-chrome-stable_current_amd64.deb https://www.dropbox.com/s/l84qhhfa4q5d59c/google-chrome-stable_current_amd64.deb?dl=1
-ls
+echo "Installing chrome browser v.88!"
 sudo apt install --allow-downgrades ./google-chrome-stable_current_amd64.deb
-echo "step1 completed!"
-echo "starting step2!"
-
-echo "step2 completed!"
-
+echo "Chrome was installed!"
