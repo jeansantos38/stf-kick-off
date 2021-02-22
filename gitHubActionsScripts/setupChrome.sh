101032cd62fb75c7ad43d1de2254b4844c8b75dd
Giacomin <giacomin@hp.com>
2021-02-22 18:13:35 -0300

    Adding more logs to script

--- gitHubActionsScripts/setupChrome.sh
+++ gitHubActionsScripts/setupChrome.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
+echo "starting step1!"
 wget https://www.dropbox.com/s/l84qhhfa4q5d59c/google-chrome-stable_current_amd64.deb?dl=1
-sudo apt install ./google-chrome-stable_current_amd64.deb
+echo "step1 completed!"
+echo "starting step2!"
+sudo apt install google-chrome-stable_current_amd64.deb
+echo "step2 completed!"
 wget https://www.dropbox.com/s/yd8w6q8jccomiev/chromedriver_linux64.zip?dl=1
 unzip chromedriver_linux64.zip
 chmod 755 chromedriver
